235d68543acbdbad5f191eb99d0ef616e955f25a
bhaggi007 <bhaggi007@gmail.com>
2025-01-24 22:46:01 +0530

    updating terraform file with security group for jenkins

--- terraform_code/4thTerraformFile.tf
+++ terraform_code/4thTerraformFile.tf
@@ -76,6 +76,13 @@ resource "aws_security_group" "dpp-securitygroup-ssh" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        description = "Allow http traffic"
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress {
         from_port = 0
         to_port = 0
